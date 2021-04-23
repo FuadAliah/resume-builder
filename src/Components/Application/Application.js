cc32ff364abbe9251d9be7edd7d02e89ea4d2ed5
Fuad Aliah <eye.design17@yahoo.com>
2021-04-23 22:33:39 +0300

    build

--- src/Components/Application/Application.js
+++ src/Components/Application/Application.js
@@ -158,13 +158,13 @@ const Application = () => {
           <div className="container mt-5">
             <Tabs defaultTab="vertical-tab-one" vertical className="vertical-tabs">
               <TabList className="me-4">
-                <Tab tabFor="vertical-tab-one" disabled={!personal.first || !personal.last || !personal.email || !personal.phone || !personal.country || !personal.birthday || !personal.marital}>
-                  Personal Information
-                </Tab>
+                <Tab tabFor="vertical-tab-one">Personal Information</Tab>
                 <Tab tabFor="vertical-tab-two" disabled={!personal.first || !personal.last || !personal.email || !personal.phone || !personal.country || !personal.birthday || !personal.marital}>
                   Educational Information
                 </Tab>
-                <Tab tabFor="vertical-tab-three">Experience Information</Tab>
+                <Tab tabFor="vertical-tab-three" disabled={!personal.first || !personal.last || !personal.email || !personal.phone || !personal.country || !personal.birthday || !personal.marital}>
+                  Experience Information
+                </Tab>
                 <Tab tabFor="vertical-tab-four" disabled={!personal.first || !personal.last || !personal.email || !personal.phone || !personal.country || !personal.birthday || !personal.marital}>
                   Additional Information
                 </Tab>
